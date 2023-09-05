ec2bcad8032474d60a3fb4180cc352f746cfdac2
Cameron Lowell Palmer <cameron@promon.no>
2023-09-05 11:38:18 +0200

    Running the OkHttp client on the main thread

--- app/src/main/java/com/cameronpalmer/okhttptest/MainActivity.kt
+++ app/src/main/java/com/cameronpalmer/okhttptest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cameronpalmer.okhttptest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cameronpalmer.okhttptest.ui.theme.OkHttpTestTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Request.Builder
+
+
 
 class MainActivity : ComponentActivity() {
+    var client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        run("https://pokeapi.co/api/v2/pokemon")
+
         setContent {
             OkHttpTestTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +40,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    fun run(url: String) {
+        val request: Request = Builder().url(url).build()
+        val call = client.newCall(request)
+        val response = call.execute()
+        Log.d("OkHttp!", response.body?.string()!!)
     }
 }
 
